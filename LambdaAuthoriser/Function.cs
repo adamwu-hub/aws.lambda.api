25578447cf35c81f47c5ec5f1c78beb1010b2a33
Adam Wu <adam.wu@outlook.com>
2022-03-04 16:50:07 +1100

    upgrade to dotnet 6

--- LambdaAuthoriser/Function.cs
+++ LambdaAuthoriser/Function.cs
@@ -1,72 +1,68 @@
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace LambdaAuthoriser
+namespace LambdaAuthoriser;
+
+public class Function
 {
-    public class Function
+
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public APIGatewayCustomAuthorizerResponse FunctionHandler(APIGatewayCustomAuthorizerRequest input, ILambdaContext context)
     {
-
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public APIGatewayCustomAuthorizerResponse FunctionHandler(APIGatewayCustomAuthorizerRequest input, ILambdaContext context)
+        try
         {
-            try
+            var body = $"Runtime: {Environment.Version} - Received `{JsonConvert.SerializeObject(input, Formatting.Indented)}` at {DateTime.UtcNow.TimeOfDay}. Context: {JsonConvert.SerializeObject(context, Formatting.Indented)}";
+
+            Console.WriteLine(body);
+
+            if (string.IsNullOrWhiteSpace(input?.AuthorizationToken))
             {
-                var body = $"Runtime: {Environment.Version} - Received `{JsonConvert.SerializeObject(input)}` at {DateTime.UtcNow.TimeOfDay}. Context: {JsonConvert.SerializeObject(context)}";
+                throw new InvalidDataException($"{nameof(input.AuthorizationToken)} cannot be null");
+            }
 
-                Console.WriteLine(body);
+            if (string.IsNullOrWhiteSpace(input?.MethodArn))
+            {
+                throw new InvalidDataException($"{nameof(input.MethodArn)} cannot be null");
+            }
 
-                var basicAuth = input?.AuthorizationToken;
-                if (string.IsNullOrWhiteSpace(basicAuth))
+            var authCtx = new APIGatewayCustomAuthorizerContextOutput();
+            authCtx["a"] = "context-a";
+            authCtx["b"] = "context-b";
+            authCtx["c"] = "context-c";
+
+            return new APIGatewayCustomAuthorizerResponse
+            {
+                PrincipalID = "wuad",
+                UsageIdentifierKey = Guid.NewGuid().ToString(),
+                Context = authCtx,
+                // related doc: https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements.html
+                PolicyDocument = new APIGatewayCustomAuthorizerPolicy
                 {
-                    throw new Exception();
-                }
-
-                var authCtx = new APIGatewayCustomAuthorizerContextOutput();
-                authCtx["a"] = "context-a";
-                authCtx["b"] = "context-b";
-                authCtx["c"] = "context-c";
-
-                return new APIGatewayCustomAuthorizerResponse
-                {
-                    PrincipalID = "wuad",
-                    UsageIdentifierKey = Guid.NewGuid().ToString(),
-                    Context = authCtx,
-                    // related doc: https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements.html
-                    PolicyDocument = new APIGatewayCustomAuthorizerPolicy
+                    //Version = "0.0.1",
+                    Statement = new List<APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement>
                     {
-                        //Version = "0.0.1",
-                        Statement = new List<APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement>
+                        new APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement
                         {
-                            new APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement
-                            {
-                                Action = new HashSet<string>{ "execute-api:Invoke" },
-                                Effect = basicAuth,
-                                Resource = new HashSet<string>{ input.MethodArn }
-                            }
+                            Action = new HashSet<string>{ "execute-api:Invoke" },
+                            Effect = input.AuthorizationToken,
+                            Resource = new HashSet<string>{ input.MethodArn }
                         }
                     }
-                };
-            }
-            catch
-            {
-                throw;
-                //return new APIGatewayCustomAuthorizerResponse
-                //{
-                //    StatusCode = 500,
-                //    Body = ex.ToString()
-                //};
-            }
+                }
+            };
+        }
+        catch
+        {
+            throw;
         }
     }
 }
--- LambdaEchoApp/Function.cs
+++ LambdaEchoApp/Function.cs
@@ -1,41 +1,57 @@
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
-using System;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace LambdaEchoApp
+namespace LambdaEchoApp;
+
+public class Function
 {
-    public class Function
-    {
 
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+    {
+        try
         {
-            try
+            var body = new RequestInfo
             {
-                var body = $"Runtime: {Environment.Version} - Received `{JsonConvert.SerializeObject(input)}` at {DateTime.UtcNow.TimeOfDay}. Context: {JsonConvert.SerializeObject(context)}";
-                return new APIGatewayProxyResponse
-                {
-                    StatusCode = 200,
-                    Body = body
-                };
-            }
-            catch (Exception ex)
+                Runtime = Environment.Version,
+                Request = input,
+                Context = context,
+                TimeSpan = DateTime.UtcNow.TimeOfDay
+            };
+
+            var json = JsonConvert.SerializeObject(body, Formatting.Indented);
+            Console.WriteLine(json);
+
+            return new APIGatewayProxyResponse
             {
-                return new APIGatewayProxyResponse
-                {
-                    StatusCode = 500,
-                    Body = ex.ToString()
-                };
-            }
+                StatusCode = 200,
+                Body = json
+            };
         }
+        catch (Exception ex)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 500,
+                Body = ex.ToString()
+            };
+        }
+    }
+
+    class RequestInfo
+    {
+        public Version Runtime { get; set; }
+        public APIGatewayProxyRequest Request { get; set; }
+        public TimeSpan TimeSpan { get; set; }
+        public ILambdaContext Context { get; set; }
     }
 }
